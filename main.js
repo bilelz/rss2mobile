fbc22ef607cbe7900300d6be2dc0d7950446b7be
bilelz <bilelz@gmail.com>
2013-02-21 15:12:12 +0100

    +bilelz blog

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function loaded() {
 }
 
 $(document).ready(function() {
-	var rss = "http://www.al-kanz.org/feed/rss/";
-	var rsscaroussel = "http://www.al-kanz.org/category/a-la-une/rss";
+	var rss = "http://feeds.feedburner.com/bilelz";
+	var rsscaroussel = "http://feeds.feedburner.com/bilelz";
 
 	if(document.location.search != ""){
 		if(document.location.search.indexOf("&caroussel=")!=-1){
@@ -28,8 +28,7 @@ $(document).ready(function() {
 			rss = document.location.search.slice(1);
 			rsscaroussel = rss;
 		}
-		console.log(rss);
-		console.log(rsscaroussel);
+		
 		
 	}
 
@@ -37,7 +36,6 @@ $(document).ready(function() {
 		url : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&callback=?&q=' + encodeURIComponent(rss),
 		dataType : 'json',
 		success : function(data) {
-			console.log(data.responseData.feed);
 
 			var tmpl = $("#items-tmpl").html(), html = "";
 			var pagestmpl = $("#pages-tmpl").html(), pageshtml = "";
@@ -55,18 +53,22 @@ $(document).ready(function() {
 					url : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&callback=?&q=' + encodeURIComponent(rsscaroussel),
 					dataType : 'json',
 					success : function(data2) {
-						console.log(data2.responseData.feed);
 	
 						var carousseltmpl = $("#caroussel-tmpl").html(), carousselhtml = "";		
 						
 						
 						var feedCaroussel = data2.responseData.feed;
-						/* set id & only one thunbmail*/
+						
 						formatRss(feedCaroussel);
 						carousselhtml = Mustache.render(carousseltmpl, feedCaroussel);
 						$('#caroussel').html("").append(carousselhtml);
 						
 						loaded();
+						
+						/* target-pages for caroussel'links */
+						pageshtml = Mustache.render(pagestmpl, feedCaroussel);
+						$('body').append(pageshtml);
+						$('.pages:not(.ui-page)').page();
 					}
 				});
 			}
@@ -75,7 +77,7 @@ $(document).ready(function() {
 			document.title = "RSS - " + feed.title;
 			$("h1:first").text(feed.title);
 			$("#description").text(feed.description);
-			$("#website").attr("href",feed.link).text(feed.link);
+			$("#website").attr("href",feed.link).find(".ui-btn-text").text(feed.link.replace("http://www.","").replace("http://",""));
 
 			html = Mustache.render(tmpl, feed);
 			pageshtml = Mustache.render(pagestmpl, feed);
@@ -156,13 +158,43 @@ $(document).ready(function() {
 
 });
 
+
+/* set unique-id & only one thunbmail/post*/
 function formatRss(_rss){
 	for(i=0;i<_rss.entries.length;i++){
-		_rss.entries[i].index = i;
+		_rss.entries[i].index = "i"+escape(_rss.entries[i].link).replace(/%/ig,"-").replace(/\//ig,"-").replace(/\./ig,"-");
+		
+		
+		
 		if(_rss.entries[i].mediaGroups != undefined && _rss.entries[i].mediaGroups.length>0 && _rss.entries[i].mediaGroups[0].contents.length>0){
 			_rss.entries[i].mediaGroups[0].contents = _rss.entries[i].mediaGroups[0].contents[0];
 		}
 		
+		if(_rss.entries[i].mediaGroups == undefined){
+			
+			/* find the first image on the article if no-enclosure  */
+			if($("<div></div>").html(_rss.entries[i].content).find("img:first").size()!=0){
+			
+				var imgsrc = $("<div></div>").html(_rss.entries[i].content).find("img:first").attr("src");
+				var img = new Object();
+	
+					img = {	contents: [{
+										url: imgsrc,
+										medium: "image",
+										title: imgsrc
+								}]
+							};				
+
+				_rss.entries[i].mediaGroups = img;
+				
+				/*console.log("img:first="+imgsrc);*/
+			}else{
+				/*console.log("no image");*/
+			}
+		}
+		
+		
+		/* boolean for print only $carousselNbItems on the caroussel */
 		if(i<carousselNbItems){
 			_rss.entries[i].incaroussel = true;
 		}
